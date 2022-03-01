b68a908ae3c7cf7e1eec661265069420fb13bc19
andrei.tataru <andrei.tataru@taulia.com>
2022-03-01 16:43:27 +0200

    updated elastic searchconfiguration

---
+++ src/main/groovy/com/taulia/supplier/onboarding/common/elasticsearch/ElasticsearchConfig.groovy
@@ -0,0 +1,29 @@
+package com.taulia.supplier.onboarding.common.elasticsearch
+
+import org.elasticsearch.client.RestHighLevelClient
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.elasticsearch.client.ClientConfiguration
+import org.springframework.data.elasticsearch.client.RestClients
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration
+
+import java.time.Duration
+
+@Configuration
+class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
+
+    @Bean
+    @Override
+    RestHighLevelClient elasticsearchClient() {
+        final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+                .connectedTo("localhost:9200")
+                .usingSsl()
+                .withConnectTimeout(Duration.ofSeconds(5))
+                .withSocketTimeout(Duration.ofSeconds(3))
+                .build()
+
+        //http://localhost:9200
+
+        return RestClients.create(clientConfiguration).rest()
+    }
+}
