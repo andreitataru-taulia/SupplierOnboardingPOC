3a10f08a5491ec070d42116886bd99b4be81792c
andrei.tataru <andrei.tataru@taulia.com>
2022-03-03 13:57:17 +0200

    added schema inferrer from json object

--- src/main/groovy/com/taulia/supplier/onboarding/mongo/supplier/SupplierController.groovy
+++ src/main/groovy/com/taulia/supplier/onboarding/mongo/supplier/SupplierController.groovy
@@ -1,5 +1,6 @@
 package com.taulia.supplier.onboarding.mongo.supplier
 
+import com.fasterxml.jackson.databind.JsonNode
 import groovy.transform.Canonical
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @Canonical
@@ -27,5 +30,10 @@ class SupplierController {
     @GetMapping("api/v0/suppliers")
     Page<Supplier> getSuppliersPaginated(Pageable pageable) {
         return supplierService.getAllSuppliersPaginated(pageable)
+    }
+
+    @PostMapping("api/v0/inferredObject")
+    JsonNode createInferredObject(@RequestBody Object object) {
+        return supplierService.getInferredSchema(object)
     }
 }
