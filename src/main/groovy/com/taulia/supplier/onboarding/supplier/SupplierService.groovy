1f1fa61565ebd1fe5ef9a084d73e01cddfcdfc9d
Cezara Cocis <cezara.cocis@taulia.com>
2022-04-08 14:49:50 +0300

    STAR-3513 update jpa configs

--- src/main/groovy/com/taulia/supplier/onboarding/supplier/SupplierService.groovy
+++ src/main/groovy/com/taulia/supplier/onboarding/supplier/SupplierService.groovy
@@ -6,8 +6,7 @@ import org.json.JSONTokener
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.json.JSONObject
-
-import javax.transaction.Transactional
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 @Transactional
